b4b2df7995147ff418e905facff809e3910bbfa3
Santiago Cardona <santiagocardo80@gmail.com>
2019-09-13 18:06:32 -0500

    Add validation search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,11 @@
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
-    StockQuote::Stock.new(api_key: "pk_182622b8fe75452f9c72836021fc73e4")
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    begin
+      StockQuote::Stock.new(api_key: "pk_182622b8fe75452f9c72836021fc73e4")
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
   end
 end
