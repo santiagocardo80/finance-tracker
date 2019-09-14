4d43d3da472dfc22ce867c6f2315f82e534e6a57
Santiago Cardona <santiagocardo80@gmail.com>
2019-09-14 18:02:21 -0500

    Add many to many association User and Stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.new_from_lookup(ticker_symbol)
     begin
       StockQuote::Stock.new(api_key: "pk_182622b8fe75452f9c72836021fc73e4")
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'user_stocks/create'
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+
+  resources :user_stocks, only: [:create]
 end
