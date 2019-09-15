21274abe9ab69fc352d2684392aff805c5f4e43c
Santiago Cardona <santiagocardo80@gmail.com>
2019-09-15 13:28:23 -0500

    Update views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'user_stocks/create'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'user/registrations' }
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
